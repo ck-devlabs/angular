7f9ebff32e23f5755835c3ae1bf2cfde401d6304
ck-devlabs <116240301+ck-devlabs@users.noreply.github.com>
2022-10-27 23:33:22 -0400

    ngClass, fade message

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class="alert alert-success" [hidden]="displayNotification">
+  template: `<div class="alert alert-success" [ngClass]="{fadeOutMe: displayNotification}">
                 <p class="notification-p">This website uses cookies</p>
                 <div class="close">
                     <button 
@@ -15,7 +15,10 @@ import { Component, OnInit } from '@angular/core';
               </div>`,
   styles: ['div{margin: 10px 0px; padding: 10px 20px; text-align:center}',
     '.notification-p{font-size:15px}',
-    '.close{float:right; margin-top:-50px}']
+    '.close{float:right; margin-top:-50px}',
+    '.fadeOutMe{opacity:0; -webkit-transition: opacity 0.15s linear; -moz-transition: opacity 0.15s linear; -o-transition: opacity 0.15s linear; transition: opacity 0.15s linear; }']
+
+
 })
 export class NotificationComponent implements OnInit {
 
